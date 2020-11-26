1fd68c4e17865abaced458aba59245de9cb5d251
couscousdude <youwenw@gmail.com>
2020-11-25 16:27:03 -0800

    Added navbar

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import './App.css';
 import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
 import Home from './components/Home';
 import Chat from './components/Chat';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <Router>
+      <div style={{marginBottom: 60}}>
+        <Navbar />
+      </div>
       <Switch>
         <Redirect from='/' to='/home' exact />
         <Route path='/home' component={Home} />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import { Typography, Button, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+    header: {
+        textAlign: 'center',
+        fontWeight: 500
+    },
+});
 
 function Home(props) {
+    const classes = useStyles();
     const history = useHistory();
+
+    const handleChatRedirect = () => {
+        history.push('/chat');
+    }
 
     return(
         <div>
-            <h1 style={{ textAlign: 'center', marginTop: 20 }}>React Video Chat</h1>
-            <button 
-                style={{ marginTop: 40 }}
-                onClick={() => {
-                    history.push('/chat');
-                }}
-            >
-                Go to Video
-            </button>
+            <Typography variant='h1' className={classes.header}>
+                React Video Chat
+            </Typography>
+            <Grid container justify='center' alignItems='center'>
+                <Grid item>
+                    <Button variant='contained' color='primary' onClick={handleChatRedirect}>
+                        Go to Chat
+                    </Button>
+                </Grid>
+            </Grid>
         </div>
     )
 }
