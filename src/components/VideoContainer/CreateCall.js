3230b615d52b7a5142f045139061dd1586eeae06
Webdev323 <ethan.z.shu@gmail.com>
2021-02-10 16:30:27 -0800

    added create call component

--- src/components/VideoContainer/CreateCall.js
+++ src/components/VideoContainer/CreateCall.js
@@ -1,10 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Typography } from '@material-ui/core';
+import { Typography, TextField, Button } from '@material-ui/core';
 
 function CreateCall(props) {
+    const [username, setUsername] = React.useState('');
+    const [meetingName, setMeetingName] = React.useState('');
+    const [meetingCode, setMeetingCode] = React.useState('');
+
+    const handleMeetingNameChange = (event) => {
+        setMeetingName(event.target.value);
+    }
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    }
+    const handleMeetingCodeChange = (event) => {
+        setMeetingCode(event.target.value);
+    }
+    const handleButtonPressed = () => {
+        
+    }
+
     return(
-        <Typography variant='subtitle1'>Create Meeting</Typography>
+        <div>
+            <Typography variant='subtitle1'>Create Meeting</Typography>
+            <Textfield variant='outlined' autoFocus label='Meeting Name' onChange={handleMeetingNameChange} />
+            <Textfield variant='outlined' label='Username' onChange={handleUsernameChange} />
+            <TextField variant='outlined' label='Meeting Code (optional)' onChange={handleMeetingCodeChange} />
+            <Button variant='filled' onClick={handleButtonPressed}>
+                Create
+            </Button>
+        </div>
     );
 }
 
