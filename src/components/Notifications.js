1ebcb69a87974d786396e97bc7de3d279bfb5339
Webdev323 <ethan.z.shu@gmail.com>
2021-05-05 16:44:38 -0700

    Updated notifications

--- src/components/Notifications.js
+++ src/components/Notifications.js
@@ -16,12 +16,18 @@ const Notifications = () => {
 
     return (
         <>
-            <Snackbar open={open}>
-                <MuiAlert severity="primary">
-                    {message}
-                </MuiAlert>
-            </Snackbar>
-            <button onClick={() => {sendNotif('code')}}>test</button>
+            <Button variant="outlined" onClick={handleClick}>
+  Open success snackbar
+</Button>
+<Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+  <Alert onClose={handleClose} severity="success">
+    This is a success message!
+  </Alert>
+</Snackbar>
+<Alert severity="error">This is an error message!</Alert>
+<Alert severity="warning">This is a warning message!</Alert>
+<Alert severity="info">This is an information message!</Alert>
+<Alert severity="success">This is a success message!</Alert>
         </>
     );
 }
